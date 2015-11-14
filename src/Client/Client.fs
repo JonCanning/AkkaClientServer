d5146f006e9501f799af815f02a7d3fff061f8ab
Jon Canning <jon.canning@gmail.com>
2015-11-14 21:18:28 +0000

    initial commit

---
+++ src/Client/Client.fs
@@ -0,0 +1,23 @@
+﻿module Client
+
+open Akka.FSharp
+open System
+
+let config = """
+  akka {
+    actor {
+      provider = "Akka.Remote.RemoteActorRefProvider, Akka.Remote"
+      serialization-bindings { "System.Object" = wire }
+      }
+    remote {
+      helios.tcp {
+        port = 8000
+        hostname = localhost
+      }
+    }
+  }
+  """
+let system = Configuration.parse config |> System.create "client"
+let server = system.ActorSelection "akka.tcp://server@localhost:8001/user/server"
+let send msg = server.Ask(msg, TimeSpan.FromSeconds 5.) |> Async.RunSynchronously
+let dispose = system.Dispose >> system.AwaitTermination
---
+++ src/Tests/ping pong.fs
@@ -0,0 +1,17 @@
+﻿module ``ping pong``
+
+open Messages
+
+[<SetUp>]
+let setUp() = Server.start()
+
+[<TearDown>]
+let tearDown() = 
+  Client.dispose()
+  Server.dispose()
+
+[<Test>]
+let ``ping returns pong``() = Client.send Ping == Pong
+
+[<Test>]
+let ``pong returns ping``() = Client.send Pong == Ping
