62ac3befa2c89d9f1e0ead176622980f402d8f40
Jon Canning <jon.canning@gmail.com>
2015-11-28 20:01:12 +0000

    remove Messages project

---
+++ src/Server/Messages.fs
@@ -0,0 +1,43 @@
+﻿module Messages
+
+open System
+
+type Token = Guid
+type Angle = float
+type Length = int
+type Sides = int
+
+type TurtleCommand = 
+  | Turn of Angle
+  | Move of Length
+  | Polygon of Sides * Length
+  | Color of byte * byte * byte
+
+type Request = 
+  | Register
+  | TurtleCommand of Token * TurtleCommand
+  | Ping of Token
+
+type Response = 
+  | Registered of Token
+  | TurtleCommandExecuted
+  | Pong of Token
+  | UnknownToken of Token
+
+type Message = 
+  | RequestMessage of Request
+  | ResponseMessage of Response
+
+let extractResponse = 
+  function 
+  | ResponseMessage response -> response
+  | _ -> failwith "Not a response"
+
+let extractRequest = 
+  function 
+  | RequestMessage request -> request
+  | _ -> failwith "Not a request"
+
+exception UnhandledMessageException of obj
+
+let unhandled o = UnhandledMessageException o |> raise
